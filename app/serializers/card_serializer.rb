76ec85250276fad690247263fdd7f079996c1598
ashtreehugs <agrev8@gmail.com>
2020-04-08 11:32:41 -0400

    Show comments recursively in Card serializer for more info

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,5 +1,4 @@
 class CardSerializer < ActiveModel::Serializer
-  attributes :id, :word, :definition, :num_likes, :num_comments
+  attributes :id, :word, :definition, :num_likes, :num_comments, :comments
   has_many :likes
-  has_many :comments
 end
